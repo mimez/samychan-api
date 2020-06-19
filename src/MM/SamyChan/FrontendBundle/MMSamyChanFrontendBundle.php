5b97906e27aaae6951c4cb8b68ba065e3212ec55
Michael Mezger <michael@michaelmezger.de>
2020-06-19 19:29:03 +0200

    Frontend-logic removed (frontend was moved to a react-repo)

--- src/MM/SamyChan/FrontendBundle/MMSamyChanFrontendBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace MM\SamyChan\FrontendBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class MMSamyChanFrontendBundle extends Bundle
-{
-}
--- src/MM/SamyChan/FrontendBundle/Resources/public/js/controllers/HeaderCtrl.js
+++
@@ -1,7 +0,0 @@
-samyChanApp.controller('HeaderCtrl', function($scope, backendUrlGenerator, nav) {
-    $scope.uploadUrl = backendUrlGenerator.buildUploadUrl();
-
-    nav.loadItems(function(items) {
-        $scope.navItems = items;
-    });
-});
